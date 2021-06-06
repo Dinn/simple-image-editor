c8815753222413c6e16d80dec2bd8647f587e446
junghyuk-park <junghyuk.park@navercorp.com>
2021-06-06 21:28:32 +0900

    Add image rendering canvas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,50 @@
+import { useState, useEffect, useRef } from "react";
+
+const MAX_CANVAS_WIDTH = 600;
+const MAX_CANVAS_HEIGHT = 800;
+
+interface size {
+  width: number;
+  height: number;
+}
+
 export default function ImageEditor() {
-  return <>Image Editor</>
+  const imageLayer = useRef<HTMLCanvasElement>(null);
+  const [imageSource, setImageSource] = useState(
+    "/rana-sawalha-X7UR0BDz-UY-unsplash.jpeg"
+  );
+
+  useEffect(() => {
+    const canvas = imageLayer.current;
+    const context = canvas?.getContext("2d");
+    const image = createImageElement(imageSource);
+    resizeCanvas(canvas, {
+      width: MAX_CANVAS_WIDTH,
+      height: MAX_CANVAS_HEIGHT,
+    });
+
+    image.onload = () => {
+      context?.drawImage(image, 0, 0, MAX_CANVAS_WIDTH, MAX_CANVAS_HEIGHT);
+    };
+  });
+
+  return (
+    <>
+      <canvas ref={imageLayer} />
+    </>
+  );
+}
+
+function createImageElement(source: string) {
+  const image = new Image();
+  image.src = source;
+  return image;
+}
+
+function resizeCanvas(
+  canvas: HTMLCanvasElement | null,
+  { width, height }: size
+) {
+  if (canvas === null) return;
+  [canvas.width, canvas.height] = [width, height];
 }
